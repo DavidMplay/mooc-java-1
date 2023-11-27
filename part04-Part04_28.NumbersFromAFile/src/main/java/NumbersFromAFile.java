93205f06a93ff34d788670c37e74be67bf6509b5
DavidMplay <vudavidmadar@gmail.com>
2023-11-27 22:20:49 +0100

    Completed Part 4 exercise 28

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,11 +1,13 @@
 
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class NumbersFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
         System.out.print("File? ");
         String file = scanner.nextLine();
@@ -13,6 +15,24 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        int i = 0;
+
+        try ( Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                list.add(reader.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        for (String item : list) {
+            int number = Integer.valueOf(item);
+            if (number >= lowerBound && number <= upperBound) {
+                i += 1;
+            } else {
+                continue;
+            }
+        }
+        System.out.println("Numbers: " + i);
 
     }
 
