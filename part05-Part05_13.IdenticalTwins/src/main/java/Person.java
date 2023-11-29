0b903c2847c01ee1ed940c31391fa7c9ad2a03da
DavidMplay <vudavidmadar@gmail.com>
2023-11-29 16:58:26 +0100

    Completed part 5 exercise 13

--- part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -16,4 +16,31 @@ public class Person {
     }
 
     // implement an equals method here for checking the equality of objects
+    public boolean equals(Object compared) {
+        // if the variables are located in the same place, they are the same.
+        if (this == compared) {
+            return true;
+        }
+
+        // if the object is not instance of SimpleDate, the objects are not the same
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+
+        // let's cast the object to SimpleDate
+        Person comparedPerson = (Person) compared;
+
+        // if the object's values are the same, the objects are the same
+        if (this.name == comparedPerson.name
+                && this.height == comparedPerson.height
+                && this.weight == comparedPerson.weight
+                && this.birthday.getYear() == comparedPerson.birthday.getYear()
+                && this.birthday.getMonth() == comparedPerson.birthday.getMonth()
+                && this.birthday.getDay() == comparedPerson.birthday.getDay()) {
+            return true;
+        }
+
+        // other wise, the objects are not the same
+        return false;
+    }
 }
